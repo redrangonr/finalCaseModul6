15918eaeba01fab420bf57cf7792b08fee8128a8
Quocanh <phiquocanh717@gmail.com>
2021-09-01 16:39:39 +0700

    tạo phôi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DetalComponent } from './detal/detal.component';
+import { AboutUSComponent } from './about-us/about-us.component';
+import { ErrorComponent } from './error/error.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetalComponent,
+    AboutUSComponent,
+    ErrorComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
