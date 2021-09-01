237561d76be2ba35a647a2248e7214181928bf48
Quocanh <phiquocanh717@gmail.com>
2021-09-02 02:01:07 +0700

    done route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {DetalComponent} from './detail/detal.component';
+import {ContactComponent} from './contact/contact.component';
+import {AboutUSComponent} from './about-us/about-us.component';
+import {ErrorComponent} from "./error/error.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'detail',
+    component: DetalComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutUSComponent
+  },
+  {
+    path: '**',
+    component: ErrorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { DetalComponent } from './detal/detal.component';
+import { DetalComponent } from './detail/detal.component';
 import { AboutUSComponent } from './about-us/about-us.component';
 import { ErrorComponent } from './error/error.component';
 import { ContactComponent } from './contact/contact.component';
