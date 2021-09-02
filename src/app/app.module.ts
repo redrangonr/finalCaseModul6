8498e776a9a7b402b42fb83a48e419d7e926c6b7
vuthanh199826 <vuthanh199825@gmail.com>
2021-09-03 01:15:43 +0700

    build create post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,16 @@ import { AboutUSComponent } from './about-us/about-us.component';
 import { ErrorComponent } from './error/error.component';
 import { ContactComponent } from './contact/contact.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {HttpClientModule} from '@angular/common/http';
+import {PostModule} from './post/post.module';
+import {EditorModule} from '@tinymce/tinymce-angular';
+import {environment} from '../environments/environment';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireModule} from '@angular/fire';
 
 
+
+// noinspection AngularInvalidImportedOrDeclaredSymbol
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +31,12 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    PostModule,
+    EditorModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'cloud')
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/post.ts
@@ -0,0 +1,10 @@
+export interface Post {
+  id?: number;
+  title?: string;
+  image?: string;
+  status?: string;
+  description?: string;
+  content?: string;
+  date?: any;
+  user?: any;
+}
