d450dd742c52d4f2b1884d559b4f9eb705c09a01
ADMIN <duccanhh94@gmail.com>
2021-09-03 21:07:21 +0700

    qly hash tag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DetalComponent } from './detail/detal.component';
-import { AboutUSComponent } from './shared-module/about-us/about-us.component';
-import { ErrorComponent } from './shared-module/error/error.component';
 import { ContactComponent } from './contact/contact.component';
 import { AdminComponent } from './admin/component/admin-home/admin.component';
 import {AdminModule} from './admin/module/admin.module';
 import { LoginComponent } from './authentication/component/login/login.component';
 import {LoginModule} from './authentication/module/login.module';
-import { UserManagementComponent } from './admin/component/user/user-list/user-management.component';
-import { NavbarComponent } from './shared-module/navbar/navbar.component';
-import { UserEditComponent } from './admin/component/user/user-edit/user-edit.component';
-import { HashtagListComponent } from './admin/component/hashtag/hashtag-list/hashtag-list.component';
-import { HashtagCreateComponent } from './admin/component/hashtag/hashtag-create/hashtag-create.component';
-import { HashtagDeleteComponent } from './admin/component/hashtag/hashtag-delete/hashtag-delete.component';
-import { HashtagEditComponent } from './admin/component/hashtag/hashtag-edit/hashtag-edit.component';
 import { AdsComponent } from './shared-module/ads/ads.component';
 import { SlideHashtagComponent } from './shared-module/slide-hashtag/slide-hashtag.component';
 import {NgxPaginationModule} from 'ngx-pagination';
@@ -35,10 +26,7 @@ import {NgbNavModule} from '@ng-bootstrap/ng-bootstrap';
     AppComponent,
     HomeComponent,
     DetalComponent,
-    AboutUSComponent,
-    ErrorComponent,
     ContactComponent,
-    NavbarComponent,
     AdsComponent,
     SlideHashtagComponent,
     HotNewComponent,
@@ -47,7 +35,6 @@ import {NgbNavModule} from '@ng-bootstrap/ng-bootstrap';
     RecentPostComponent,
     SignupEmailAdsComponent,
     UserDetailComponent,
-
   ],
     imports: [
         BrowserModule,
