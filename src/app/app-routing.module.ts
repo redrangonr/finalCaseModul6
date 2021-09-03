154d0342517535dc2391bdc0f286a7be54df83e9
ADMIN <duccanhh94@gmail.com>
2021-09-04 01:50:08 +0700

    bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {DetalComponent} from './detail/detal.component';
 import {ContactComponent} from './contact/contact.component';
-import {AboutUSComponent} from './shared-module/about-us/about-us.component';
-import {ErrorComponent} from './shared-module/error/error.component';
 
 const routes: Routes = [
   {path: 'admin', loadChildren: () => import('./admin/module/admin.module').then(module => module.AdminModule)},
@@ -21,10 +19,7 @@ const routes: Routes = [
     path: 'contact',
     component: ContactComponent
   },
-  {
-    path: 'about',
-    component: AboutUSComponent
-  },
+
   // {
   //   path: '**',
   //   component: ErrorComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DetalComponent } from './detail/detal.component';
-import { AboutUSComponent } from './shared-module/about-us/about-us.component';
-import { ErrorComponent } from './shared-module/error/error.component';
 import { ContactComponent } from './contact/contact.component';
-<<<<<<< HEAD
-import { NavbarComponent } from './navbar/navbar.component';
 
-=======
 import { AdminComponent } from './admin/component/admin-home/admin.component';
 import {AdminModule} from './admin/module/admin.module';
 import { LoginComponent } from './authentication/component/login/login.component';
@@ -24,27 +19,22 @@ import { HashtagCreateComponent } from './admin/component/hashtag/hashtag-create
 import { HashtagDeleteComponent } from './admin/component/hashtag/hashtag-delete/hashtag-delete.component';
 import { HashtagEditComponent } from './admin/component/hashtag/hashtag-edit/hashtag-edit.component';
 import { AdsComponent } from './shared-module/ads/ads.component';
->>>>>>> 3c46790da246efe6b74d543c98a41500ca370f89
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     DetalComponent,
-    AboutUSComponent,
-    ErrorComponent,
     ContactComponent,
     NavbarComponent,
-<<<<<<< HEAD
-=======
-    AdsComponent,
-
->>>>>>> 3c46790da246efe6b74d543c98a41500ca370f89
+    AdsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LoginModule,
+    ReactiveFormsModule,
   ],
   providers: [],
     exports: [
