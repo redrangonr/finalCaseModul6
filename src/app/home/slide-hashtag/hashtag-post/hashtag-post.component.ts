86b00ce0323d6da6030699ecd880c8e87629804b
vuthanh199826 <vuthanh199825@gmail.com>
2021-09-08 22:26:01 +0700

    merge list

--- src/app/home/slide-hashtag/hashtag-post/hashtag-post.component.ts
+++ src/app/home/slide-hashtag/hashtag-post/hashtag-post.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {PostService} from '../../../service/post.service';
 import {Post} from '../../../model/post';
 import {Hashtag} from '../../../admin/model/hashtag';
 import {HashtagService} from '../../../admin/service/hashtag.service';
 import {ActivatedRoute, ParamMap} from '@angular/router';
+import {PostService} from '../../../post/service/post.service';
 
 @Component({
   selector: 'app-hashtag-post',
@@ -17,46 +17,51 @@ export class HashtagPostComponent implements OnInit {
   tableSize = 10 ;
   tableSizesArr = [4, 8, 12];
   currentIndex = 1;
-  hashtags: Hashtag[] = []
+  hashtags: Hashtag[] = [];
   tag: Hashtag = {} ;
-  id: any = 0
-  constructor(private postService: PostService, private hashtagService: HashtagService,private activateRoute: ActivatedRoute ) {
-    this.activateRoute.paramMap.subscribe((data: ParamMap)=>{
+  id: any = 0;
+  constructor(private postService: PostService, private hashtagService: HashtagService, private activateRoute: ActivatedRoute ) {
+    this.activateRoute.paramMap.subscribe((data: ParamMap) => {
       // @ts-ignore
-      this.id = +data.get('id')
-      this.getPost(this.id)
-      this.getAllHasshtag()
-      this.findHashtagbyId(this.id)
-    })
+      this.id = +data.get('id');
+      this.getPost(this.id);
+      this.getAllHasshtag();
+      this.findHashtagbyId(this.id);
+    });
   }
   // getHashtag(id: any){
   //   this.hashtagService.findById(id).subscribe(data=>{
   //     this.tag = data;
   //   })
   // }
+  // tslint:disable-next-line:typedef
   reload(){
 
   }
+  // tslint:disable-next-line:typedef
   getPost(id: any){
-    this.postService.findAllByHashtag(id).subscribe(data =>{
+    this.postService.findAllByHashtag(id).subscribe( data => {
       // @ts-ignore
-      this.posts = data
-    })
+      this.posts = data;
+    });
   }
+  // tslint:disable-next-line:typedef
   getAllHasshtag(){
-    this.hashtagService.getAll().subscribe(data=>{
+    this.hashtagService.getAll().subscribe(data => {
       // @ts-ignore
-      this.hashtags = data
-    })
+      this.hashtags = data;
+    });
   }
+  // tslint:disable-next-line:typedef
    findHashtagbyId(id: any){
-    this.hashtagService.findById(id).subscribe(tag=>{
-      this.tag = tag
-      console.log(this.tag.image)
-    })
+    this.hashtagService.findById(id).subscribe(tag => {
+      this.tag = tag;
+      console.log(this.tag.image);
+    });
    }
   ngOnInit(): void {
   }
+  // tslint:disable-next-line:typedef
   tabSize(event: any) {
     this.page = event;
   }
