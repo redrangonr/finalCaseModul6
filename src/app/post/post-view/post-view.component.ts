f300eab67fffc1c211c63258c99e06d8a779e150
baotoan-codegym <thaibaotoanht1992@gmail.com>
2021-09-11 22:51:48 +0700

    ggg

--- src/app/post/post-view/post-view.component.ts
+++ src/app/post/post-view/post-view.component.ts
@@ -14,6 +14,7 @@ import {AngularFireStorage} from '@angular/fire/storage';
   styleUrls: ['./post-view.component.css']
 })
 export class PostViewComponent implements OnInit {
+  idUser = sessionStorage.getItem('Id_key');
   postInstand: Post = {
     user: {},
     hashtag: {},
